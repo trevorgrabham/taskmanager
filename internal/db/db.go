23d383f2bd056ce61f44570a8e5022a78c009874
Trevor Grabham <trevarus1997@gmail.com>
2026-02-11 15:43:39 -0800

    updated: combined setup into the db.go file

--- internal/db/db.go
+++ internal/db/db.go
@@ -19,6 +19,27 @@ type QueryParams struct {
 	Category string 
 }
 
+func Setup(db *sql.DB) error {
+	if db == nil {
+		return fmt.Errorf("setting up: cannot setup a nil db")
+	}
+	_, err := db.Exec(fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	title TEXT NOT NULL, 
+	category TEXT, 
+	description TEXT,
+	due_date INTEGER NOT NULL,
+	completion_date INTEGER NOT NULL DEFAULT -1,
+	done INTEGER NOT NULL DEFAULT 0
+		CHECK (done IN (0, 1))
+);`, taskTableName))
+	if err != nil {
+		return fmt.Errorf("executing setup: %s", err)
+	}
+
+	return nil
+}
+
 func Connect() (*sql.DB, error) {
 	db, err := sql.Open("sqlite3", dbFileName)
 	if err != nil {
